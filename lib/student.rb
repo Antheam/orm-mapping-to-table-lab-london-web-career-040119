b30e4015f1747b1943b856ec6fe36bd8882a442a
Anthea Merton <anthea.merton@gmail.com>
2019-04-16 13:28:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,6 @@ class Student
 def initialize(name, grade, id=nil)
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
-  
 end
 
 def self.create_table
